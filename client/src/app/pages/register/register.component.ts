67a57b857dcede09c310b92b15aab6218c5351e6
MegaDraconius <ivanvclinetor@gmail.com>
2024-05-24 07:57:50 +0200

    Give styles on register components

--- client/src/app/pages/register/register.component.ts
+++ client/src/app/pages/register/register.component.ts
@@ -1,34 +1,44 @@
 import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { UserService } from '../../services/user.service';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FormsModule],
+  imports: [
+    FormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatInputModule,
+    RouterLink,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
-  userServcie = inject(UserService)
+  userServcie = inject(UserService);
   signUpObj: SignUpModel = new SignUpModel();
 
-  onRegister(){
-    
+  onRegister() {
     console.log(this.signUpObj);
-    this.userServcie.addUser(this.signUpObj).subscribe()
+    this.userServcie.addUser(this.signUpObj).subscribe();
   }
-
 }
 
-export class SignUpModel{
+export class SignUpModel {
   userName: string;
   email: string;
   password: string;
 
-  constructor(){
-    this.userName = "";
-    this.email = "";
-    this.password = "";
+  constructor() {
+    this.userName = '';
+    this.email = '';
+    this.password = '';
   }
 }
