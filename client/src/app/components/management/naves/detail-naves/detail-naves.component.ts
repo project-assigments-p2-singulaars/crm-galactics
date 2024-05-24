e44e870aada1c10675cdee98042993fe3d3ab4d8
MegaDraconius <ivanvclinetor@gmail.com>
2024-05-24 10:59:00 +0200

    Add styles on detail-naves-components

--- client/src/app/components/management/naves/detail-naves/detail-naves.component.ts
+++ client/src/app/components/management/naves/detail-naves/detail-naves.component.ts
@@ -2,36 +2,36 @@ import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../../../../services/crud.service';
 import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
 import { Nave } from '../../../../interfaces/database';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-detail-naves',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatButtonModule],
   templateUrl: './detail-naves.component.html',
-  styleUrl: './detail-naves.component.scss'
+  styleUrl: './detail-naves.component.scss',
 })
-export class DetailNavesComponent implements OnInit{
+export class DetailNavesComponent implements OnInit {
+  constructor(
+    private crudService: CrudService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-
-  constructor(private crudService: CrudService, private route: ActivatedRoute,private router: Router) { }
-
-  
   naves: Nave[] = [];
   nave!: Nave;
   newCustomer: any = {};
   editmode = false;
 
   ngOnInit(): void {
-      
     const id = this.route.snapshot.paramMap.get('id');
 
     if (id) {
-      this.crudService.getNaveById(+id).subscribe(nave => {
-        this.nave = nave;        
+      this.crudService.getNaveById(+id).subscribe((nave) => {
+        this.nave = nave;
       });
     }
   }
-
 
   deleteNave(id: number) {
     this.crudService.deleteNave(id).subscribe();
@@ -41,7 +41,4 @@ export class DetailNavesComponent implements OnInit{
   editMode() {
     this.editmode = !this.editmode;
   }
-
-  
-
 }
