00584294120837396b617355847ad2c1c23825bc
MegaDraconius <ivanvclinetor@gmail.com>
2024-05-24 12:50:09 +0200

    Edit the components naves,dashboard and register

--- client/src/app/components/management/naves/list-naves/list-naves.component.ts
+++ client/src/app/components/management/naves/list-naves/list-naves.component.ts
@@ -2,36 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { CrudService } from '../../../../services/crud.service';
 import { Customer, Nave, Order } from '../../../../interfaces/database';
 import { RouterLink } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list-naves',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatButtonModule],
   templateUrl: './list-naves.component.html',
-  styleUrl: './list-naves.component.scss'
+  styleUrl: './list-naves.component.scss',
 })
-export class ListNavesComponent implements OnInit{
-
-
+export class ListNavesComponent implements OnInit {
   customers: Customer[] = [];
   naves: Nave[] = [];
   orders: Order[] = [];
   customer!: Customer;
   newCustomer: any = {};
-  nave!:Nave;
+  nave!: Nave;
 
   ngOnInit(): void {
-      this.getNaves();
+    this.getNaves();
   }
 
-    constructor(private crudService: CrudService) {
-      
-    }
+  constructor(private crudService: CrudService) {}
 
   getNaves(): void {
-    this.crudService.getNaves().subscribe(naves => {
+    this.crudService.getNaves().subscribe((naves) => {
       this.naves = naves;
     });
   }
-
 }
